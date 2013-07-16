3fe4d3ffde88a0c35249417341caa04e3a154088
Rick Jones <rick@edna-2.local>
2013-07-16 13:19:08 +0100

    Updated vim manifest to install and run Vundle.

--- manifests/vim.pp
+++ manifests/vim.pp
@@ -1,9 +1,23 @@
 class vim {
   file { "${home}/.vimrc":
+    require => [
+      File["${home}/.vim"], 
+      Repository["${home}/.vim/bundle/vundle"]
+    ],
     source => "puppet:///modules/habitullence/vim/.vimrc",
   }
 
   file { "${home}/.vim":
     source => "puppet:///modules/habitullence/vim/.vim",
+  }
+
+  repository { "${home}/.vim/bundle/vundle":
+    require => File["${home}/.vim"],
+    source => 'gmarik/vundle',
+  }
+
+  exec { 'Install Vundle bundles':
+    command => 'vim +BundleInstall +qall',
+    require => Repository["${home}/.vim/bundle/vundle"],
   }
 }
