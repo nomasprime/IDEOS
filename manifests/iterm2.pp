27335f4244552c8faea83d5f78a94a6822fdafea
Rick Jones <rick.jones@playtimestudios.com>
2013-07-24 15:47:03 +0100

    Don't want to just overwrite preferences file.

--- manifests/iterm2.pp
+++ manifests/iterm2.pp
@@ -1,7 +1,3 @@
 class habitullence::iterm2($home) {
   include iterm2::dev
-
-  file { "${home}/Library/Preferences/com.googlecode.iterm2.plist":
-    source => "puppet:///modules/habitullence/iterm2/com.googlecode.iterm2.plist",
-  }
 }
